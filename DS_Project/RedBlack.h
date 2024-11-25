978266fdc3df1fa474da2d8a3cdaa83328bce26a
Taha <taharizwan492@gmail.com>
2024-11-25 18:44:07 +0500

    Red Black Tree Deletion Implemented

--- DS_Project/RedBlack.h
+++ DS_Project/RedBlack.h
@@ -10,17 +10,14 @@ template<class T>
 struct RedBlackNode {
     T data;
     bool color;
+    int occurences;
     //0 for black, 1 for red
     RedBlackNode<T>* left, * right, * parent;
 
-    RedBlackNode(T data)
-        : data(data)
-        , color(RED)    
-        , left(nullptr)
-        , right(nullptr)
-        , parent(nullptr)
-    {
-    }
+    RedBlackNode(T data): data(data), color(RED), left(nullptr), right(nullptr), parent(nullptr) , occurences(1)
+    {}
+    RedBlackNode() :  color(BLACK), left(nullptr), right(nullptr), parent(nullptr), occurences(1)
+    {}
 };
 
 // Red-Black Tree class
@@ -28,6 +25,7 @@ template<class T>
 class RedBlackTree {
 private:
     RedBlackNode<T>* root;
+    RedBlackNode<T>* nil;
 
 
     int isEqual(char c, char d) {
@@ -168,68 +166,16 @@ private:
         root ->color = BLACK;
     }
 
-public:
-    RedBlackTree() {
-        root = nullptr;
-    }
-
-   
-    void insert(T data) {
-        cout << data << endl;
-        if (root == nullptr) {
-            root = new RedBlackNode<T>(data);
-            root->color = BLACK;
-            return;
-        }
-        else {
-            RedBlackNode<T>* curr = root;
-            RedBlackNode<T>* par = nullptr;
-            while (curr) {
-                par = curr;
-                if (isEqual(data, curr->data) == 1) {
-                    curr = curr->right;
-                }
-                else {
-                    curr = curr->left;
-                }
-            }
-            RedBlackNode<T>* yo;
-            //if greater or equal add to right
-            if (isEqual(data, par->data)==1||isEqual(data,par->data)==0) {
-                par->right = new RedBlackNode<T>(data);
-                par->right->parent = par;
-                yo = par->right;
-            }
-            else {
-                par->left = new RedBlackNode<T>(data);
-                par->left->parent = par;
-                yo = par->left;
-            }
-
-            if (yo->parent == nullptr) {
-                yo->color = BLACK;
-                return;
-            }
-              
-            if (yo->parent->parent == nullptr)
-                return;
-
-
-            change(yo);
-
-        }
-    }
-
     void inorderHelper(RedBlackNode<T>* node) {
-        if (node != nullptr) {
+        if (node != nullptr && node!=nil) {
             inorderHelper(node->left);
             cout << node->data << " ";
-          
+
             inorderHelper(node->right);
         }
     }
     void printTree(RedBlackNode <T>* node, int space = 0, int indent = 4) {
-        if (node == nullptr) {
+        if (node == nullptr||node==nil) {
             return;
         }
 
@@ -244,10 +190,245 @@ public:
         for (int i = indent; i < space; ++i) {
             std::cout << " ";
         }
-        std::cout << node->data << "\n";
+        std::cout << node->data<<"!" << node->color << "\n";
 
         // Process left child
         printTree(node->left, space);
+    }
+
+
+
+    void transplant(RedBlackNode<T>* x, RedBlackNode<T>* y) {
+        if (!x->parent||x->parent==nil) {
+            //X is root
+            root = y;
+        }
+        else if (x == x->parent->left) {
+            //x is on the left of parent
+            x->parent->left = y;
+        }
+        else {
+            //x is on the right of parent
+            x->parent->right = y;
+        }
+         y->parent = x->parent;
+    }
+
+    RedBlackNode<T>* searchHelper(RedBlackNode<T>* node, int n) {
+        if (!node || node==nil)
+            return nullptr;
+
+
+        if (isEqual(n,node->data)==1)
+            return searchHelper(node->right, n);
+        else if (isEqual(n, node->data) == -1) {
+            return searchHelper(node->left, n);
+        }
+        else {
+            return node;
+        }
+
+    }
+
+    void fixDelete(RedBlackNode<T>* node) {
+        //While node is not root and node is black
+        RedBlackNode<T>* sibling;
+        while (node != root && !node->color) {
+            //if node is at left of parent
+            if (node == node->parent->left) {
+                sibling = node->parent->right;
+                //Sibling is red(Case 1)
+                if (sibling->color) {
+                    sibling->color = BLACK;
+                    node->parent->color = RED;
+                    rotateLeft(node->parent);
+                    sibling = node->parent->right;
+                }
+                //Sibling is black and has two black children (Case 2)
+                if (!sibling->left->color && !sibling->right->color) {
+                    sibling->color = RED;
+                    node = node->parent;
+                }
+                //Sibling is black and left child is red right child is black (Case 3)
+                else if (!sibling->right->color) {
+                    sibling->left->color = BLACK;
+                    sibling->color = RED;
+                    rotateRight(sibling);
+                    //Going to grandpapi
+                    node = node->parent->parent;
+                    sibling = node->parent->right;
+                   
+                }
+                //Sibling is black and right child is red (Case 4)
+                sibling->color = node->parent->color;
+                node->parent->color = BLACK;
+                
+                sibling->right->color = BLACK;
+
+               
+                rotateLeft(node->parent);
+                node = root;
+            }
+            //if node is at right of parent
+            else {
+                sibling = node->parent->left;
+                //Sibling is red(Case 1)
+                if (sibling->color) {
+                    sibling->color = BLACK;
+                    node->parent->color = RED;
+                    rotateRight(node->parent);
+                    //display();
+                    sibling = node->parent->left;
+                }
+                //Sibling is black and has two black children (Case 2)
+                if (!sibling->right->color && !sibling->left->color) {
+                    sibling->color = RED;
+                    node = node->parent;
+                }
+                //Sibling is black and left child is red right child is black (Case 3)
+                else if (!sibling->left->color) {
+                    sibling->right->color = BLACK;
+                    sibling->color = RED;
+                    rotateLeft(sibling);
+                  
+                    //Going to grandpapi
+                    node = node->parent->parent;
+                    sibling = node->parent->left;
+                }
+             
+                sibling->color = node->parent->color;
+                node->parent->color = BLACK;
+              
+                sibling->left->color = BLACK;
+                rotateRight(node->parent);
+                node = root;
+            }
+        }
+        if(node)
+            node->color = BLACK;
+    }
+
+    void deleteNode(RedBlackNode<T>* node) {
+        RedBlackNode<T>* y = node;
+        bool origColor = y->color;
+        RedBlackNode<T>* x;
+        //One child only case(right) + no child case
+        if (node->left == nullptr||node->left==nil) {
+    
+            x = node->right;
+            transplant(node, node->right);
+        }
+        //One child only case(left)
+        else if (node->right == nullptr || node->right == nil) {
+            x = node->left;
+            transplant(node, node->left);
+        }
+        //two child case
+        else {
+            
+            y = findMin(node->right);
+            origColor = y->color;
+            x = y->right;
+            if (y->parent == node) {
+                x->parent = y;
+            }
+            else {
+                transplant(y, y->right);
+                y->right = node->right;
+                y->right->parent = y;
+            }
+            transplant(node, y);
+            y->left = node->left;
+            y->left->parent = y;
+            y->color = node->color;
+        }
+        //In case original color is black there will be violations
+        if (!origColor)
+            fixDelete(x);
+    }
+
+    RedBlackNode<T>* findMin(RedBlackNode<T>* node) {
+        while (node->left&& node->left != nil) {
+            node = node->left;
+        }
+        return node;
+    }
+
+public:
+    RedBlackTree() {
+        root = nullptr;
+        nil = new RedBlackNode<T>();
+    }
+
+   
+    void insert(T data) {
+        cout << data << endl;
+        if (root == nullptr) {
+            root = new RedBlackNode<T>(data);
+            root->color = BLACK;
+            root->left = nil;
+            root->right = nil;
+            return;
+        }
+        else {
+            RedBlackNode<T>* curr = root;
+            RedBlackNode<T>* par = nullptr;
+            while (curr&&curr!=nil) {
+                par = curr;
+                if (isEqual(data, curr->data) == 0) {
+                    curr->occurences++;
+                    return;
+                }
+                else if (isEqual(data, curr->data) == 1) {
+                    curr = curr->right;
+                }
+                else {
+                    curr = curr->left;
+                }
+            }
+            RedBlackNode<T>* yo;
+            //if greater or equal add to right
+            if (isEqual(data, par->data) == 0) {
+                par->occurences++;
+                return;
+            }
+            else if (isEqual(data, par->data)==1) {
+                par->right = new RedBlackNode<T>(data);
+                par->right->parent = par;
+                yo = par->right;
+            }
+            else {
+                par->left = new RedBlackNode<T>(data);
+                par->left->parent = par;
+                yo = par->left;
+            }
+            yo->left = nil;
+            yo->right = nil;
+            if (yo->parent == nullptr) {
+                yo->color = BLACK;
+                return;
+            }
+              
+            if (yo->parent->parent == nullptr)
+                return;
+
+
+            change(yo);
+
+        }
+    }
+    RedBlackNode<T>* search(int val) {
+        return searchHelper(root, val);
+    }
+
+    void deleteByVal(int val) {
+        RedBlackNode<T>* x = search(val);
+        if (!x||x==nil) {
+            return;
+        }
+        else {
+            deleteNode(x);
+        }
     }
 
     void display() {
--- DS_Project/source.cpp
+++ DS_Project/source.cpp
@@ -4,24 +4,61 @@ using namespace std;
 
 
 int main() {
-	CSVReader j;
 
-	RedBlackTree<string> r;
-	j.readCSVColumn(1, r);
-	/*r.insert(30);
-	r.insert(62);
-	r.insert(76);
-	r.insert(28);
-	r.insert(43);
-	r.insert(36);
-	r.insert(21);
-	r.insert(20);*/
-	//r.insert(82);
-	//r.insert(58);
-	//r.insert(72);
-	//r.insert(38);
+	RedBlackTree<int> r;
+
+	r.insert(10);
+	r.insert(20);
+	r.insert(30);
+	r.insert(40);
+	r.insert(50);
+	r.insert(35);
+	r.deleteByVal(30);
 	r.display();
+	RedBlackTree<int> t;
+	
+	t.insert(10);
+	t.insert(20);
+	t.insert(30);
+	t.insert(40);
+	t.deleteByVal(10);
+	t.display();
+	RedBlackTree<int> p;
 
+	p.insert(10);
+	p.insert(20);
+	p.insert(30);
+	p.insert(40);
+	p.deleteByVal(10);
+	p.insert(10);
+	p.deleteByVal(40);
+
+	p.insert(10);
+	p.insert(20);
+	p.insert(30);
+	p.insert(40);
+	p.deleteByVal(10);
+	p.insert(10);
+	p.deleteByVal(40);
+	p.insert(80);
+	p.deleteByVal(10);
+	p.display();
+
+
+	RedBlackTree<int> k;
+	k.insert(65);
+	k.insert(50);
+	k.insert(80);
+	k.insert(90);
+	k.insert(70);
+	k.insert(60);
+	k.insert(10);
+	k.insert(62);
+	k.deleteByVal(90);
+	k.deleteByVal(80);
+	k.deleteByVal(70);
+	//k.deleteByVal(20);
+	k.display();
 	return 0;
 }
 
